c331dd07e6a836fa9abc0317be72817c3ce3f20b
keizie <github@keizie.net>
2020-11-24 03:18:51 +0900

    first table migration with devtool

--- app/config/config.php
+++ app/config/config.php
@@ -10,10 +10,10 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 return new \Phalcon\Config([
     'database' => [
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'host'        => 'docker-mysql',
+        'username'    => 'sample',
+        'password'    => 'sample',
+        'dbname'      => 'sample',
         'charset'     => 'utf8',
     ],
     'application' => [
---
+++ app/migrations/1.0.0/order.php
@@ -0,0 +1,93 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class OrderMigration_100
+ */
+class OrderMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('order', [
+                'columns' => [
+                    new Column(
+                        'order_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 1,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'oid',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 12,
+                            'after' => 'order_id'
+                        ]
+                    ),
+                    new Column(
+                        'product',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'oid'
+                        ]
+                    ),
+                    new Column(
+                        'datetime',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'notNull' => true,
+                            'after' => 'product'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['order_id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'table_type' => 'BASE TABLE',
+                    'auto_increment' => '',
+                    'engine' => 'InnoDB',
+                    'table_collation' => 'utf8mb4_0900_ai_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
